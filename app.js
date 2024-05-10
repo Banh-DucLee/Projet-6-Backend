9b9261093c489969e16b3b160da9920abb2a21ae
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-10 10:07:39 +0200

    Initial commit with working node server on port 4000 & express app connected to mongoDB

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://pekaizilla:0YX5dWz7hOZUE0id@monvieuxgrimoire.pdxm7fc.mongodb.net/?retryWrites=true&w=majority&appName=MonVieuxGrimoire', 
+    {   useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(() => console.log('Error connecting to MongoDB'));
+
+app.use(express.json());
+
+// Set Response Header
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+module.exports = app;
