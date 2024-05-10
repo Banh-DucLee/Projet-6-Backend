6b2603217343e2e4d185f65bda10ce84e324bffb
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-10 12:44:11 +0200

    Renamed file to auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,6 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10).then(hash => {
