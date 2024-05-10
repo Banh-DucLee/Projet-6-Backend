dcc7a7e12f529f444d257cbc00cc00545a426496
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-10 15:21:11 +0200

    Fixed indent for auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,9 @@ module.exports = (req, res, next) => {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
         const userId = decodedToken.userId;
-        req.auth = {userId: userId};
+        req.auth = {
+            userId: userId
+        };
         next();
     } catch (error) {
         res.status(401).json({error});
