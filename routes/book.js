765515e79d4e3b4837e455fd23cd391bac868a7c
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-11 13:48:49 +0200

    Moved /bestrating routes

--- routes/book.js
+++ routes/book.js
@@ -5,9 +5,9 @@ const auth = require('../middlewares/auth');
 const multer = require('../middlewares/multer-config');
 const bookCtrl = require('../controllers/book');
 
+router.get('/bestrating', bookCtrl.getBestRating);
 router.get('/', bookCtrl.getAllBooks);
 router.get('/:id', bookCtrl.getBook);
-router.get('/bestrating', bookCtrl.getBestRating);
 router.post('/', auth, multer, bookCtrl.createBook);
 router.put('/:id', auth, multer, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
