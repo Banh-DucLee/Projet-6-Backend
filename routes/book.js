76a222fc71dba888a7f8c30976e2f83b1bae2153
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-12 10:56:59 +0200

    Using newly created upload middleware

--- routes/book.js
+++ routes/book.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middlewares/auth');
-const multer = require('../middlewares/multer-config');
+const { upload, processImage } = require('../middlewares/upload');
 const bookCtrl = require('../controllers/book');
 
 router.get('/bestrating', bookCtrl.getBestRating);
 router.get('/', bookCtrl.getAllBooks);
 router.get('/:id', bookCtrl.getBook);
-router.post('/', auth, multer, bookCtrl.createBook);
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.post('/', auth, upload.single('image'), processImage, bookCtrl.createBook);
+router.put('/:id', auth, upload.single('image'), processImage, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.post('/:id/rating', auth, bookCtrl.rateBook);
 
