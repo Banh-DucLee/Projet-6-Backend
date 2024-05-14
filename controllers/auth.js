ba82bdb708858947516c7ebff78a613284f88ed0
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-14 12:03:47 +0200

    Adapted errors code

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,7 +13,7 @@ exports.signup = (req, res, next) => {
                 message: 'User created successfully'
             })
         }).catch(error => {
-            res.status(400).json({
+            res.status(409).json({
                 error
             })
         })
@@ -27,11 +27,15 @@ exports.signup = (req, res, next) => {
 exports.login = (req, res, next) => {
     User.findOne({email: req.body.email}).then(user => {
         if(!user) {
-            return res.status(401).json({message: 'Incorrect login/password'});
+            return res.status(404).json({
+                error
+            });
         }
         bcrypt.compare(req.body.password, user.password).then(valid => {
             if(!valid) {
-                return res.status(401).json({message: 'Incorrect login/password'});
+                return res.status(404).json({
+                    error
+                });
             }
             res.status(200).json({
                 userId: user._id,
@@ -42,12 +46,12 @@ exports.login = (req, res, next) => {
                 )
             });
         }).catch(error => {
-            res.status(500).json({
+            res.status(404).json({
                 error
             })
         })
     }).catch(error => {
-        res.status(500).json({
+        res.status(404).json({
             error
         })
     })
