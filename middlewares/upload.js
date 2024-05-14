aa6ecb513e3547b688b7e0157b58f06a2e870308
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-14 13:17:53 +0200

    Checked file extension to allow only image formats

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -6,6 +6,15 @@ const upload = multer({ storage: storage });
 
 function processImage(req, res, next) {
     const { buffer, originalname } = req.file;
+
+    const fileExtenion = req.file.originalname.split('.').pop().toLowerCase();
+
+    if (!['png', 'jpg', 'jpeg', 'gif'].includes(fileExtenion)) {
+        return res.status(422).json({
+            error: 'Only .png, .jpg, .jpeg, .gif are allowed' 
+        })
+    }
+
     const name = originalname.split('.').slice(0, -1).join('.');
     const ref = `${new Date().toISOString()}_${name.split(' ').join('_')}.webp`;
     sharp(buffer).webp({quality: 20}).toFile(`images/${ref}`, (err) => {
