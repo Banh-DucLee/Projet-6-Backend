671be77cb97d185f3ffe5b402a808215bb5d78c5
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-15 13:01:58 +0200

    Fixed variableAuth name

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { validateAuth} = require('../middlewares/validation');
+const { validateAuth } = require('../middlewares/validation');
 
 const userCtrl = require('../controllers/auth');
 
