d22f385052fd0b6a5179d92e1979c04a9bdd061b
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-15 13:01:36 +0200

    Renamed error variable

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,31 +1,35 @@
 const multer = require('multer');
 const sharp = require('sharp');
+const fs = require('fs');
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 function processImage(req, res, next) {
-    const { buffer, originalname } = req.file;
+    if (req.file) {
+        const { buffer, originalname } = req.file;
 
-    const fileExtenion = req.file.originalname.split('.').pop().toLowerCase();
-
-    if (!['png', 'jpg', 'jpeg', 'webp'].includes(fileExtenion)) {
-        return res.status(422).json({
-            error: 'Only .png, .jpg, .jpeg, .webp are allowed' 
-        })
-    }
-
-    const name = originalname.split('.').slice(0, -1).join('.');
-    const ref = `${new Date().toISOString()}_${name.split(' ').join('_')}.webp`;
-    sharp(buffer).webp({quality: 20}).toFile(`images/${ref}`, (err) => {
-        if (err) {
-            return res.status(500).json({ error: 'Something went wrong' });
+        const fileExtenion = req.file.originalname.split('.').pop().toLowerCase();
+    
+        if (!['png', 'jpg', 'jpeg', 'webp'].includes(fileExtenion)) {
+            return res.status(422).json({
+                error: 'Only .png, .jpg, .jpeg, .webp are allowed' 
+            })
         }
-
-        req.file.filename = ref;
-
-        next();
-    });
+    
+        const name = originalname.split('.').slice(0, -1).join('.');
+        const ref = `${new Date().toISOString()}_${name.split(' ').join('_')}.webp`;
+        sharp(buffer).webp({quality: 20}).toFile(`images/${ref}`, (error) => {
+            if (error) {
+                fs.unlinkSync(`images/${ref}`);
+                return res.status(500).json({ error });
+            }
+    
+            req.file.filename = ref;
+    
+            next();
+        });
+    }
 }
 
 module.exports = { upload, processImage };
